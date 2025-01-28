76ef1037a0f18fec4ef66e42e9283ff8e689296a
mohsen1299@gmail.com <mohsen1299@gmail.com>
2025-01-28 19:21:41 +0330

    make simple controller for invoice

--- src/invoice/invoice.controller.ts
+++ src/invoice/invoice.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { InvoiceService } from './invoice.service';
 
 @Controller('invoice')
-export class InvoiceController {}
+export class InvoiceController {
+  constructor(private readonly invoiceService: InvoiceService) {}
+
+  // Create a new invoice.
+  @Post()
+  create(@Body() createDto: any) {
+    return this.invoiceService.create(createDto);
+  }
+
+  // Retrieve a specific invoice by ID
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.invoiceService.findOne(id);
+  }
+
+  // Retrieve a list of all invoices (with optional filters like date range).
+  @Get()
+  findAll() {
+    return this.invoiceService.findAll();
+  }
+}
