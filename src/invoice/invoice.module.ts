8a4870d723ff73bbe101db9cae6b0496603f9a6a
mohsen1299@gmail.com <mohsen1299@gmail.com>
2025-01-28 19:04:55 +0330

    Connect invoice Schema to Mongoose Module

--- src/invoice/invoice.module.ts
+++ src/invoice/invoice.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { InvoiceService } from './invoice.service';
 import { InvoiceController } from './invoice.controller';
+import { invoice, InvoiceSchema } from './schemas/invoice.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: invoice.name, schema: InvoiceSchema }]),
+  ],
   providers: [InvoiceService],
   controllers: [InvoiceController]
 })
